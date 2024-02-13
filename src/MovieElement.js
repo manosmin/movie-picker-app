920fa6b917e05eff3d9ab9837f604b834f479319
manosmin <manosmin@hotmail.com>
2024-02-13 17:58:41 +0200

    added movies

---
+++ src/MovieElement.js
@@ -0,0 +1,26 @@
+import Card from 'react-bootstrap/Card'
+
+function MovieElement(props) {
+    const styles = { 
+        width: '16rem', 
+        marginRight: '2rem', 
+        marginLeft: '2rem', 
+        marginTop: '2rem', 
+        marginBottom: '1rem'
+    }
+
+    return (<>
+            <Card style={styles}>
+                <Card.Img variant="top" src={props.myResults.Poster} />
+                <Card.Body>
+                    <Card.Title>{props.myResults.Title}</Card.Title>
+                    <Card.Text>{props.myResults.Year}</Card.Text>
+                    <Card.Text>{props.myResults.imdbID}</Card.Text>
+                    <Card.Text>{props.myResults.Type}</Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+      )
+}
+
+export default MovieElement
---
+++ src/Movies.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import MovieElement from './MovieElement';
+
+function Movies() {
+    const API_KEY = '7b24a2fe';
+    const [userInput, setUserInput] = useState('');
+    const [movieResults, setMovieResults] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        sendQuery();
+    }, [currentPage]);
+
+    function sendQuery() {
+        const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${userInput}&page=${currentPage}`;
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(json => setMovieResults(json))
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <div>
+            <div style={{ textAlign: 'center', marginTop: '2rem'}}>
+                <input
+                    style={{ marginRight: '0.5rem' }}
+                    placeholder="Search"
+                    onChange={(e) => setUserInput(e.target.value)}
+                ></input>
+                <button onClick={sendQuery}>🔎</button>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', maxWidth: '100%'}} className="row">
+                {movieResults && movieResults.Response !== 'False' &&
+                    movieResults.Search.slice(0, 5).map(item => (
+                        <MovieElement myResults={item}></MovieElement>
+                    ))}
+            </div>
+            {movieResults && movieResults.Response !== 'False' && (
+                <div style={{ textAlign: 'center', marginTop: '1rem' }}>
+                    <button style={{ marginRight: '0.5rem' }} disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>
+                        ⬅
+                    </button>
+                    <button onClick={() => setCurrentPage(currentPage + 1)}>
+                        ➡
+                    </button>
+                </div>
+            )}
+
+        </div>
+    );
+}
+
+export default Movies;
